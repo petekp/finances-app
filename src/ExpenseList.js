bae3d0f18e669a3c9efc2f3505b9332d47d21b8b
petekp <pete.petrash@gmail.com>
2016-10-10 20:09:49 -0700

    no parens needed

--- src/ExpenseList.js
+++ src/ExpenseList.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {ExpenseListItem} from './ExpenseListItem'
 
-const renderItems = (props) => (
+const renderItems = (props) =>
   Object.keys(props).map((prop, i) => {
     const data = props[prop]
     return (<ExpenseListItem {...data} key={i} />)
   })
-)
 
 export const ExpenseList = props =>
   <div className="expense-list">
